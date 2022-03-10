8b49bc5378d206f3a2408e08d9144ad0e0df725e
MikeSSJ <74191867+MIkeSSJ@users.noreply.github.com>
2022-03-10 16:23:45 -0300

    Add files via upload

---
+++ java-io/src/br/com/alura/java/io/teste/TesteEscritaPrintStreamPrintWriter.java
@@ -0,0 +1,31 @@
+package br.com.alura.java.io.teste;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TesteEscritaPrintStreamPrintWriter {
+
+	public static void main(String[] args) throws IOException {
+
+		// Fluxo de entrada com Arquivo
+
+//		OutputStream fos = new FileOutputStream("lorem2.txt");
+//		Writer osw = new OutputStreamWriter(fos);
+//		BufferedWriter bw = new BufferedWriter(osw);
+
+//		BufferedWriter bw = new BufferedWriter(new FileWriter("lorem2.txt"));
+
+//		PrintStream ps = new PrintStream(new File("lorem2.txt"));
+
+		PrintWriter ps = new PrintWriter(new File("lorem2.txt", "UTF-8"));
+		ps.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+		ps.println();
+		ps.println("asfasdfsafdas dfs sdf asf assdﬂ");
+
+		ps.close();
+		
+		
+		
+	}
+}
